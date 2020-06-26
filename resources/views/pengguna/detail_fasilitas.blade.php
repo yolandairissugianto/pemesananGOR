7c1de80b0ad85b52bb4db32475341bd2ef8d9726
Damar Permadany <damarp2017@gmail.com>
2020-06-26 16:21:59 +0700

    change ui on pemesanan

--- resources/views/pengguna/detail_fasilitas.blade.php
+++ resources/views/pengguna/detail_fasilitas.blade.php
@@ -3,82 +3,90 @@
 <title>Detail Fasilitas</title>
 
 @section('content')
-<section id="page-title">
-    <div class="container clearfix">
-        <h1>Fasilitas</h1>
-        <span>Aritkel berkaitan dengan GOR Trisanja</span>
-    </div>
-</section>
+    <section id="page-title">
+        <div class="container clearfix">
+            <h1>Fasilitas</h1>
+            <span>Aritkel berkaitan dengan GOR Trisanja</span>
+        </div>
+    </section>
 
-<div class="content-wrap">
-    <div class="container clearfix">
-        <div class="single-post nobottommargin">
-            <div class="entry clearfix">
-                <div class="entry-title">
-                    <h1>{{ $facility-> nama_fasilitas }}</h1>
-                </div>
+    <div class="content-wrap">
+        <div class="container clearfix">
+            <div class="single-post nobottommargin">
+                <div class="entry clearfix">
+                    <div class="entry-title">
+                        <h1>{{ $facility-> nama_fasilitas }}</h1>
+                    </div>
 
-                <div class="entry-image bottommargin">
-                    <img src="{{ asset('uploads/admin/fasilitas/'. $facility -> gambar) }}" width="auto" height="auto" alt="">
-                </div>
+                    <div class="entry-image bottommargin">
+                        <img src="{{ asset('uploads/admin/fasilitas/'. $facility -> gambar) }}" width="auto"
+                             height="auto" alt="">
+                    </div>
 
-                <div class="entry-content notopmargin">
-                    <h4>{{ $facility -> deskripsi }}</h4>
-                    <p> 
-                    </p>
-                    <table class="table table-striped">
-                        <thead>
+                    <div class="entry-content notopmargin">
+                        <h4>{{ $facility -> deskripsi }}</h4>
+                        <p>
+                        </p>
+                        <table class="table table-striped">
+                            <thead>
                             <tr>
                                 <th>Kegiatan</th>
                                 <th>Lama Peminjaman</th>
                                 <th>Harga</th>
                                 <th></th>
                             </tr>
-                        </thead>
-                        <tbody>
+                            </thead>
+                            <tbody>
                             <tr>
-                                <td>Penggunaan untuk olahraga siang</td>
+                                <td>
+                                    Penggunaan untuk olahraga
+                                    <br>Siang: 06.00-17.00
+                                    <br>Malam: 17.00-21.00
+                                </td>
                                 <td>1 Jam</td>
-                                <td>{{ $facility -> olahraga_siang }}</td>
-                                <td><a href="{{ route('pengguna.pemesanan_jam') }}"
-                                    class="button button-border button-rounded button-small noleftmargin ">Pesan Tempat</a></td>
-                            </tr>
-                            <tr>
-                                <td>Penggunaan untuk olahraga malam</td>
-                                <td>1 Jam</td>
-                                <td>{{ $facility -> olahraga_malam }}</td>
-                                <td><a href="{{ route('pengguna.pemesanan_jam') }}"
-                                    class="button button-border button-rounded button-small noleftmargin ">Pesan Tempat</a></td>
+                                <td>
+                                    Siang: {{ $facility -> olahraga_siang }}
+                                    <br>Malam: {{ $facility -> olahraga_malam }}
+                                </td>
+                                <td>
+                                    <a href="{{ route('pengguna.pemesanan_jam') }}"
+                                       class="button button-border button-rounded button-small noleftmargin ">Pesan
+                                        Tempat</a></td>
                             </tr>
                             <tr>
                                 <td>Penggunaan selain olahraga (Menarik Karcis dan Sponsor)</td>
                                 <td>1 Hari</td>
                                 <td>{{ $facility -> dengan_karcis_sponsor }}</td>
-                                <td><a href="{{ route('pengguna.pemesanan_hari') }}"
-                                    class="button button-border button-rounded button-small noleftmargin ">Pesan Tempat</a></td>
+                                <td>
+                                    <a href="{{ route('pengguna.pemesanan_hari') }}"
+                                       class="button button-border button-rounded button-small noleftmargin ">Pesan
+                                        Tempat</a></td>
                             </tr>
                             <tr>
                                 <td>Penggunaan selain olahraga (Dengan Sponsor)</td>
                                 <td>1 Hari</td>
                                 <td>{{ $facility -> dengan_sponsor }}</td>
-                                <td><a href="{{ route('pengguna.pemesanan_hari') }}"
-                                    class="button button-border button-rounded button-small noleftmargin ">Pesan Tempat</a></td>
+                                <td>
+                                    <a href="{{ route('pengguna.pemesanan_hari') }}"
+                                       class="button button-border button-rounded button-small noleftmargin ">Pesan
+                                        Tempat</a></td>
                             </tr>
                             <tr>
                                 <td>Penggunaan selain olahraga (Tanpa Karcis dan Sponsor)</td>
                                 <td>1 Hari</td>
                                 <td>{{ $facility -> tanpa_karcis_sponsor }}</td>
                                 <td><a href="{{ route('pengguna.pemesanan_hari') }}"
-                                    class="button button-border button-rounded button-small noleftmargin">Pesan Tempat</a></td>
+                                       class="button button-border button-rounded button-small noleftmargin">Pesan
+                                        Tempat</a></td>
                             </tr>
-                        </tbody>
-                    </table>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
+
+                <!-- <button name="submit" type="submit" id="submit-button" tabindex="5" value="Submit"
+                    class="button button-3d nomargin">Pesan Tempat</button> -->
             </div>
-            
-            <!-- <button name="submit" type="submit" id="submit-button" tabindex="5" value="Submit"
-                class="button button-3d nomargin">Pesan Tempat</button> -->
         </div>
     </div>
-</div>
 @endsection
